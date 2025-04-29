641bd67018399e98b9f279439a128bfcb856c793
sega-playstation <brainkrang@outlook.com>
2025-04-29 06:26:00 -0500

    updated hero section to respond to mobile view

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ export default function Hero() {
         </h2>
       </section>
 
-      <div className="w-full max-w-xl mx-auto mt-6 mb-4">
+      <div className="w-full max-w-xl mx-auto mt-6 mb-4 px-4 sm:px-6 md:px-8">
         <div className="text-left">
           <p className="font-bold text-md">
             My name is Stephen Kryworuchka — a full-stack web developer and DevOps technician with a diploma in Business Information Technology from Red River College Polytechnic. I also hold an Autodesk AutoCAD certification (2018–2019) from the Manitoba Institute of Trades and Technology.
